d75eba56f8493e2b085d081b2517881ce2133bc7
Fulvio Corno <fulvio.corno@polito.it>
2019-04-09 10:21:55 +0200

    Base project

---
+++ src/it/polito/tdp/model/Model.java
@@ -0,0 +1,14 @@
+package it.polito.tdp.model;
+
+import java.util.List;
+
+public class Model {
+
+	public List<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
+		
+		System.out.println("TODO!");
+		
+		return null;
+	}
+
+}
---
+++ src/it/polito/tdp/model/TestModel.java
@@ -0,0 +1,13 @@
+package it.polito.tdp.model;
+
+public class TestModel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Model model = new Model();
+		model.calcolaSottoinsiemeEsami(20);
+		
+	}
+
+}
